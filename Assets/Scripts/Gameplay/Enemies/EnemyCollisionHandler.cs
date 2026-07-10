fc0e404862982df0b93eb707cf11a402c69a5b88
ViktorB <zeinridan@gmail.com>
2026-07-10 11:23:18 +0200

    Small changes in CollideWithPlayer functionality

--- Assets/Scripts/Gameplay/Enemies/EnemyCollisionHandler.cs
+++ Assets/Scripts/Gameplay/Enemies/EnemyCollisionHandler.cs
@@ -25,27 +25,28 @@ namespace Gameplay.Enemies
 
         protected void OnCollisionEnter2D(Collision2D other)
         {
-            CollideWithPlayer(other);
-        }
-
-        private void CollideWithPlayer(Collision2D other)
-        {
             if (!other.gameObject.TryGetComponent(out Player _)) return;
 
             _audioService.PlayCollision();
 
-            try
+            CollideWithPlayer(other).Forget(exception =>
             {
-                _enemyMovement.ChangeMoveDirection((transform.position - other.transform.position).normalized).Forget();
-                _enemyMovement.ChangeMoveSpeed().Forget();
-                _enemyMovement.RotateAfterCollision().Forget();
-            }
-            catch (OperationCanceledException e)
-            {
-                Debug.Log(e.Message);
-            }
+                if (exception is OperationCanceledException)
+                    return;
+
+                Debug.LogException(exception);
+            });
 
             _signalBus.Fire(new PlayerCollidedSignal(gameObject));
+        }
+
+        private async UniTask CollideWithPlayer(Collision2D other)
+        {
+            await UniTask.WhenAll(
+                _enemyMovement.ChangeMoveDirection((transform.position - other.transform.position).normalized),
+                _enemyMovement.ChangeMoveSpeed(),
+                _enemyMovement.RotateAfterCollision()
+            );
         }
     }
 }
